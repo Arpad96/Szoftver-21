d43fd0ca0f3e02ddf77070e366de26b5f867edf4
Arpad30 <74504516+Arpad30@users.noreply.github.com>
2021-11-23 14:46:10 +0200

    Add simple text

--- app/src/main/java/com/example/simplequiz/MainActivity.kt
+++ app/src/main/java/com/example/simplequiz/MainActivity.kt
@@ -18,5 +18,6 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         //return super.onSupportNavigateUp()
         return navController.navigateUp()
+        // TODO: 11/23/2021 remove comments 
     }
 }
