e9fa6223d3d630df918ae8dd3d4b091e89ec98cf
Arpad30 <74504516+Arpad30@users.noreply.github.com>
2021-11-23 15:05:54 +0200

    Removed unused comments.

--- app/src/main/java/com/example/simplequiz/MainActivity.kt
+++ app/src/main/java/com/example/simplequiz/MainActivity.kt
@@ -18,7 +18,5 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         //return super.onSupportNavigateUp()
         return navController.navigateUp()
-        // TODO: 11/23/2021 remove comments
-        // TODO: 11/23/2021 Added another line 
     }
 }
